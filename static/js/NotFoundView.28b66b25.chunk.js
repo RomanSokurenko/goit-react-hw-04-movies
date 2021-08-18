f5d973a7298e29c387dbfbb97f0a0a5829f3abeb
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-08-18 21:03:35 +0300

    Updates

---
+++ static/js/NotFoundView.28b66b25.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[4],{46:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return o}));var i=n(1);function o(){return Object(i.jsx)("h1",{children:"404 Page not found"})}}}]);
+//# sourceMappingURL=NotFoundView.28b66b25.chunk.js.map
